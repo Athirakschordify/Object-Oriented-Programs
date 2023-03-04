d71d83d722e703867dd4a20d38b653261cebe5d8
user <athira.ks@chordify.com>
2023-03-04 23:27:37 +0530

    employe manager pgm

--- emp_class.rb
+++ emp_class.rb
@@ -1,3 +1,7 @@
+# 1. A company has two types of employees: regular employees and managers. All employees have a name and a salary, but managers also have a bonus percentage that is added to their salary.
+
+# Write a program that creates two classes, Employee and Manager, where Manager inherits from Employee. The Employee class should have methods to get and set the name and salary, and the Manager class should have an additional method to get and set the bonus percentage.
+
 class Employee
 	def initialize(name1,salary,days)
 		@name=name1
